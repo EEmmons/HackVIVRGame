11acfa5eda76486d339d66a844767c04d88e7465
EEmmons <eemmons@umass.edu>
2018-10-13 17:33:34 -0400

    working disk

---
+++ HackVIVRGame/Assets/DiskMotion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiskMotion : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float thrust;
+
+	// Use this for initialization
+	void Start () {
+		//rb.AddForce(0, 2000, 5000);
+		rb = GetComponent<Rigidbody>();
+		//thrust = rb.AddForce(0, 2000, 5000);
+		rb.AddForce(0, 0, thrust, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//rb.AddForce(0, 0, thrust, ForceMode.Impulse);
+	}}
---
+++ HackVIVRGame/Assets/MonkeyMotion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonkeyMotion : MonoBehaviour {
+
+	public Rigidbody rb;
+	public float thrust;
+
+	// Use this for initialization
+	void Start () {
+		//rb.AddForce(0, 2000, 5000);
+		rb = GetComponent<Rigidbody>();
+		//thrust = rb.AddForce(0, 2000, 5000);
+		rb.AddForce(0, 0, thrust, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//rb.AddForce(0, 0, thrust, ForceMode.Impulse);
+	}
+}
